7069432adc3e32c7bfe9230b7ea6bbb4d61200d7
rsa951 <RSA95183@gmail.com>
2022-11-07 23:48:01 +0300

    add shahad test

--- singleton.java
+++ singleton.java
@@ -4,6 +4,7 @@ public class singleton {
 		
 		 
 		private static singleton ins; 
+		private int shaconfig; 
 		
 		private singleton() {
 			System.out.print("shahad abdullah");
@@ -19,7 +20,20 @@ public class singleton {
 		return ins;
 	
 }
+		/*
+		 * this function is to set an int number to shaconfig
+		 * @param con1 value to set new value
+		 */
+		public void setShconfig(int con1) {
+			assert(shaconfig % 1 == 0)
+			shaconfig=con1;
+			} 	
+		/*
+		 * this function is to get an int number of shaconfig
+		 * @return shaconfig value 
+		 */
 		
-		
-		
+		public int getShconfig() {
+		return shaconfig;	
+		} 	
 }
