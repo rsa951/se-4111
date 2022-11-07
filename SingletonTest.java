915a9e45490ae42b34f3c253bd2a3b41cd0595b8
rsa951 <RSA95183@gmail.com>
2022-11-08 01:09:30 +0300

    add shahad's test

--- SingletonTest.java
+++ SingletonTest.java
@@ -1,4 +1,17 @@
+import static org.junit.Assert.*;
 
 public class SingletonTest {
+	
+	 static singleton ShahadSn; 
+	 
+@Before
+	 public void SetUpclass() {
+		 ShahadSn = singleton.getInstance();
+	 }
+@Test	
+public void TestSetconfig() {
+	 ShahadSn.setShconfig(20);
+	 assertEquals(20,ShahadSn.getShconfig);
+}
 
 }
