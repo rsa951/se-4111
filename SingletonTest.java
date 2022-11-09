7eb7339794cf147af2e5b45af087814eefb9fe49
rsa951 <RSA95183@gmail.com>
2022-11-09 21:29:53 +0300

    add Rema test

--- SingletonTest.java
+++ SingletonTest.java
@@ -11,20 +11,24 @@ class SingletonTest {
 	
 	static Singleton ShahadSn;
 	 static Singleton AshwaqSn1; 
-	
+	 static Singleton RemaSn2; 
+	 
 	@BeforeEach
 	static void setUpBeforeClass()  {
 		ShahadSn = Singleton.getInstance();
 		AshwaqSn1 = Singleton.getInstance();
+		RemaSn2 = Singleton.getInstance();
 		 	
 	}
-
-	@Test
+	
+@Test
 	void testSetShconfig() {
 		ShahadSn.setShconfig(20);
 		 assertEquals(20,ShahadSn.getShconfig());
 		 AshwaqSn1.setAshconfig(40);
 		 assertEquals(40,AshwaqSn1.getAshconfig);
+		 RemaSn2.setReconfig(60);
+		 assertEquals(60,RemaSn2.getReconfig);
 		 
 	}
 
