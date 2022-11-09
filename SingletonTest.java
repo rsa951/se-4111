6163aede77426d0db5daa50afa915e0a264087ad
rsa951 <RSA95183@gmail.com>
2022-11-09 22:34:48 +0300

    add Rema test

--- SingletonTest.java
+++ SingletonTest.java
@@ -1,36 +1,31 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
 class SingletonTest {
-	
 	static Singleton ShahadSn;
 	 static Singleton AshwaqSn1; 
 	 static Singleton RemaSn2; 
-	 
-	@BeforeEach
+@BeforeEach
 	static void setUpBeforeClass()  {
 		ShahadSn = Singleton.getInstance();
 		AshwaqSn1 = Singleton.getInstance();
-		RemaSn2 = Singleton.getInstance();
-		 	
-	}
-	
+		RemaSn2 = Singleton.getInstance(); }
 @Test
 	void testSetShconfig() {
 		ShahadSn.setShconfig(20);
 		 assertEquals(20,ShahadSn.getShconfig());
+		 }
+@Test
+      void testSetAshconfig() {
 		 AshwaqSn1.setAshconfig(40);
 		 assertEquals(40,AshwaqSn1.getAshconfig);
-		 RemaSn2.setReconfig(60);
+		 }
+@Test
+    void testSetReconfig() {
+    	 RemaSn2.setReconfig(60);
 		 assertEquals(60,RemaSn2.getReconfig);
-		 
-	}
-
+	 
 }
-
-
+}
