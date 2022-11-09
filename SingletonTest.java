1cb954043a0a7332598fa72743d248e41a43408d
96654 <96654@192.168.1.109>
2022-11-09 21:20:31 +0300

    add Ashwaq test

--- SingletonTest.java
+++ SingletonTest.java
@@ -9,21 +9,24 @@ import lll.Sc;
 
 class SingletonTest {
 	
-	static Singleton ShahadSn; 
+	static Singleton ShahadSn;
+	 static Singleton AshwaqSn1; 
 	
 	@BeforeEach
 	static void setUpBeforeClass()  {
 		ShahadSn = Singleton.getInstance();
-		
+		AshwaqSn1 = Singleton.getInstance();
+		 	
 	}
-
 
 	@Test
 	void testSetShconfig() {
 		ShahadSn.setShconfig(20);
 		 assertEquals(20,ShahadSn.getShconfig());
+		 AshwaqSn1.setAshconfig(40);
+		 assertEquals(40,AshwaqSn1.getAshconfig);
+		 
 	}
-
 
 }
 
