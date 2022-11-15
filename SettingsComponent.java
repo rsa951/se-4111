27bcd6f771f2195f5588c7cd18ba7ea0362ca86c
96654 <96654@192.168.1.109>
2022-11-15 23:35:54 +0300

    Ashwaq setting

--- SettingsComponent.java
+++ SettingsComponent.java
@@ -1,16 +1,18 @@
-public class SettingsComponent { 
+package aaa;
+
+
 	public class SettingsComponent { 
 		
 		public static void main(String[] args) {
 		
 		  // Assemble all the pieces of the MVC
-		  SettingsModel m =  SettingsModel.getInstance();
+
+		 SettingsModel m = SettingsModel.getInstance();
 		  SettingsView v = new SettingsView();
-		  SettingsController c = new SettingsController(m, v);
-		  
+		  SettingsController c=new SettingsController(m, v);
 		  c.initController();
 		  c.initControllerSh();
-	 
+		  c.initControllerASH();
 		}
 
 	}
